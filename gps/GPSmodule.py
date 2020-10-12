f8bd770b22193c54d615465373f826ff4cdf7965
Maximiliano Palay <maximilianopalay@gmail.com>
2020-10-12 11:21:01 -0300

    cambios en gps por error al abrir puerto, mejor manejo de errores

--- gps/GPSmodule.py
+++ gps/GPSmodule.py
@@ -1,5 +1,7 @@
 import serial
 from time import sleep
+import serial.tools.list_ports as lp
+GPS_HWID = "10C4:EA60"
 class GPS():
     def get_connection(): # workaround
         return
@@ -15,26 +17,39 @@ class GPS():
         self.get_connection()
     
     def get_connection(self):
-        try:
-            self.conn = serial.Serial("/dev/gps0")
-            self.no_connection = False
-        except:
-            print("cant connect to gps module")
+        ports = list(lp.comports(True))
+        gps_port = None
+        for p in ports:
+            if GPS_HWID in p.hwid:
+                gps_port = p.device
+        if gps_port is not None:
+            try:
+                self.conn = serial.Serial(gps_port)#gps0")
+                self.no_connection = False
+                print("connected to GPS module")
+            except:
+                print("cant connect to gps module")
+        else:
+            print("gps is not connected")
 
     def get_gps_data(self):
         if self.conn:
             for i in range(1, self.attempts):
-                line = self.conn.readline()
-                data = line.split(",")
+                try:
+                    line = self.conn.readline()
+                except:
+                    self.conn = None
+                    break
+                data = line.decode(encoding='us-ascii').split(',')
                 if data[0] == "$GPRMC":
                     if data[2] == "A":
                         self.valido = 0
                     self.lat = (float(data[3]))/100
                     if data[4] == "S":
-                        self.lat = -lat
-                    lon = (float(data[5]))/100
+                        self.lat = -self.lat
+                    self.lon = (float(data[5]))/100
                     if data[6]=="W":
-                        self.lon = -lon
+                        self.lon = -self.lon
                     self.speed = float(data[7])*1.85
                     break
         else:
